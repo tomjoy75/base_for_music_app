eceb5f2ae78706a985f65a5cea9c19998f52ab0f
tomjoy75 <tomjoy75@gmail.com>
2025-06-16 11:29:53 +0200

    feat: fetch and display random advice on GameBoard mount

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,7 +1,27 @@
 // Zone principale du jeu
 
+import { useState, useEffect } from "react"
+
 export function GameBoard() {
+	// let response: any;
+	const [msg, setMsg] = useState("");
+
+	useEffect(() => {
+		async function startFetching() {
+			const res = await fetch('https://api.adviceslip.com/advice', {method: 'GET', cache: 'no-cache'});
+			const json = await res.json();
+			setMsg(json.slip.advice);		
+			console.log(msg);
+		}
+		startFetching();
+	}, []);
+
 	return (
-		<p>GameBoard</p>
+		<>
+			<p>GameBoard</p>
+			<p>{msg}</p>
+		</>
 	)
 }
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#checking_that_the_fetch_was_successful
